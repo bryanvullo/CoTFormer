bfb033f408b6d49c634bca18cc4cd531f8882319
Aras Kavuncu <aras.kavuncu@yahoo.com>
2026-04-13 13:18:56 +0100

    will test wether this one works should do

--- iridis/tak_ablation/reserved_layers_exp/attn_small_job.sh
+++ iridis/tak_ablation/reserved_layers_exp/attn_small_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
-#SBATCH --mem=128G
+#SBATCH --mem=32G
 #SBATCH --time=24:00:00
 ################################################################################
 # CoTFormer + Reserved Layers Training -- iridis/cot-res-train
