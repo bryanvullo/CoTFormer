4151573e4f492f824aeede3018ce26182762c733
Abe <totoberni@gmail.com>
2026-04-29 19:33:03 +0100

    fix: job3 only on 1 GPU!

--- iridis/base-train/job3.sh
+++ iridis/base-train/job3.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=128G
 #SBATCH --time=24:00:00
 # ========================= CONFIGURATION ======================================
